b6079746ba60ab01b42e1ee0483de17897ca453a
Spencer Rauch <rauchs@gmail.com>
2023-06-12 06:25:45 -0700

    before lecture

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+Console.WriteLine("\n\n**Let's learn LINQ**\n\n");
+
+List<int> numbers = new List<int>()
+{
+  5, 15, 20, 0, 1, 3, 25
+};
+
+
+
+
+
+
+List<string> names = new List<string>()
+{
+  "Neil", "Mark", "Lauren", "Jake", "Adam", "Garrett"
+};
+
+
+
+
+
+
+List<VideoGame> games = new List<VideoGame> {
+    new VideoGame("Apex Legends", "Riot", "E", 0, "Xbox"),
+    new VideoGame("The Last of Us", "Naughty Dog", "M", 39.99, "PlayStation"),
+    new VideoGame("Untitled Goose Game", "House House", "E", 29.99, "PC"),
+    new VideoGame("Super Mario Bros.", "Nintendo", "E", 49.99, "SNES"),
+    new VideoGame("Elden Ring", "FromSoftware", "M", 59.99, "PC"),
+    new VideoGame("World of Warcraft", "Blizzard", "E", 49.99, "PC"),
+    new VideoGame("Overwatch 2", "Blizzard", "T", 0, "PC")
+};
+
+
+
